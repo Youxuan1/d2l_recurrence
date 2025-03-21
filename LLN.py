441c0343aa2122ec39985e0e77909da1420edfac
You Xuan <fmstrange1@gmail.com>
2025-03-22 01:19:38 +0800

    Updated LLN.py

--- LLN.py
+++ LLN.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
 
     d2l.set_figsize((6, 4.5))
     for i in range(6):
-        d2l.plt.plot(estimates[:, i].numpy(), label=("P(die=" + str(i + 1) + ")"))
+        d2l.plt.plot(estimates[:, i].numpy(),
+                     label=("P(die=" + str(i + 1) + ")"))
 
     d2l.plt.axhline(y=0.167, color="black", linestyle="dashed")
     d2l.plt.gca().set_xlabel("Groups of experiments")
