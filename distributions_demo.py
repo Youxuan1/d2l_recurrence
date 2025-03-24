c7610edef1bef94520a87c854abad2eb1ca142b9
游轩 <fmstrange1@gmail.com>
2025-03-24 15:49:56 +0800

    Updated distributions_demo.py.py

--- distributions_demo.py
+++ distributions_demo.py
@@ -1,3 +1,21 @@
 """Utilize scipy.stats to plot famous distributions"""
 
+import numpy as np
 from scipy.stats import norm
+
+
+def dist_normal(idv: np.ndarray, loc: float = 0, scale: float = 1) -> np.ndarray:
+    """Generate Gaussian distribution."""
+
+    return norm.pdf(idv, loc, scale)
+
+
+if __name__ == "__main__":
+    x = np.arange(-10, 10, 0.1)
+    y = dist_normal(x, 2, 3)
+
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x, y, "r-", lw=5, alpha=0.6, label="Normal Distribution")
+    plt.show()
