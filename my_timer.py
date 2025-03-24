4f093de4667df445597f24c8927169c449ee43fa
游轩 <fmstrange1@gmail.com>
2025-03-24 11:45:46 +0800

    Updated my_timer.py

---
+++ my_timer.py
@@ -0,0 +1,34 @@
+"""Timer utility class for measuring runtime."""
+
+import time
+
+import numpy as np
+
+
+class Timer:
+    """Record multiple runtime"""
+
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self) -> None:
+        """Init timer"""
+        self.tik = time.time()
+
+    def stop(self) -> float:
+        """Stop timer and record it"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self) -> float:
+        """Return average runtime"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self) -> float:
+        """Return total runtime"""
+        return sum(self.times)
+
+    def cumsum(self) -> float:
+        """Return accumulative runtime"""
+        return np.array(self.times).cumsum().tolist()
